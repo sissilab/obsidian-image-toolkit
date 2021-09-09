65aabfcb9e2cb43fbc78d3c78cd3b849294cd908
dlccyes <derricken968@gmail.com>
2021-09-09 17:18:29 +0800

    Update zh-tw.ts

--- src/lang/locale/zh-tw.ts
+++ src/lang/locale/zh-tw.ts
@@ -1,3 +1,25 @@
 // 繁體中文
 
-export default {};
+export default {
+  
+  // settings
+  IMAGE_TOOLKIT_SETTINGS_TITLE: "image toolkit 設定",
+
+  VIEW_IMAGE_TOGGLE_NAME: "啟用點擊圖片以檢視",
+  VIEW_IMAGE_TOGGLE_DESC: "點擊圖片後，你可以對彈出視窗縮放、旋轉與拖曳",
+  
+  // toolbar icon title
+  ZOOM_IN: "放大",
+  ZOOM_OUT: "縮小",
+  FULL_SCREEN: '全螢幕',
+  REFRESH: "重整",
+  ROTATE_LEFT: "向左旋轉",
+  ROTATE_RIGHT: "向右旋轉",
+  SCALE_X: 'x 軸縮放',
+  SCALE_Y: 'y 軸縮放',
+  INVERT_COLOR: '色彩反轉',
+  COPY: '複製',
+
+  COPY_IMAGE_SUCCESS: '成功複製圖片！'
+
+};
