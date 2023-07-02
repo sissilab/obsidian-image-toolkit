fbc1b13e1c9bffcdbb13f691c86d39fb7a2f02f0
Ivan <82884158+sissilab@users.noreply.github.com>
2023-07-02 11:43:17 +0800

    Update en.ts

--- src/lang/locale/en.ts
+++ src/lang/locale/en.ts
@@ -8,8 +8,8 @@ export default {
   // >>>Common Settings:
   COMMON_SETTINGS: 'Common Settings:',
   VIEW_MODE_NAME: 'Choose a mode to view images',
-  VIEW_MODE_NORMAL: 'Normal',
-  VIEW_MODE_PIN: 'Pin',
+  VIEW_MODE_NORMAL: '🖼 Normal',
+  VIEW_MODE_PIN: '📌 Pin',
 
   // >>>View Trigger Settings:
   VIEW_TRIGGER_SETTINGS: 'View Trigger Settings:',
@@ -29,7 +29,7 @@ export default {
   PIN_MODE_SETTINGS: "Pin Mode Settings:",
   PIN_MODE_NAME: "📌 Pin an image",
   PIN_MODE_DESC: "You can pin an image onto the top of the screen. And have more options by right click. (press Esc to close the image where your mouse cursor is hovering)",
-  PIN_MAXIMUM_NAME: "The maximum image you can pin",
+  PIN_MAXIMUM_NAME: "The maximum images you can pin",
   PIN_COVER_NAME: "Cover mode",
   PIN_COVER_DESC: "After those pinned images reach maximum, you can cover the earliest pinned image when you click an image once again.",
 
